fb85fb343397bbdd2e6a818911d30bb14abbb649
Omar Torres <jtorres@axsistec.com>
2025-07-31 08:24:12 -0600

    prompting

---
+++ PromptsDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.SemanticKernel;
+using Shared;
+
+Console.WriteLine("Hello, World!");
+
+
+
+Kernel openAIKernel = KernelFactory.CreateKernel(TypeKernel.OpenAI);
+
+
+var prompt = "Who is Isaac Newton?";
+
+
+var questionFunction = openAIKernel.CreateFunctionFromPrompt(prompt);
+
+var result = await questionFunction.InvokeAsync(openAIKernel);
+
+
+Console.WriteLine($"Result: {result}");
+
+
